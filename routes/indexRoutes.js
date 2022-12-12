8e3ddfac75532a4192f6940bbe441582c8515dc0
Anmol Tanwar <anmol.tanwar@srvmedia.com>
2022-12-12 10:47:33 +0530

    homepage changed

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -6,5 +6,8 @@ const userRouter = require("./userRoutes");
 // split up route handling
 router.use("/users", userRouter);
 router.use("/test", testRouter);
+router.use("/", (req, res) => {
+    return res.send(200).json({message:'Visit path /users to test it'})
+});
 
 module.exports = router;
